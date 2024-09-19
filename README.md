3bcc9fe8be052ff2f9f48f679ffb7a96a756a308
pedrohpso <pedro.soares@aluno.ifsp.edu.br>
2024-09-19 20:45:13 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# tcc-sistema-egressos
-Sistema de egressos 
+# Sistema de egressos 
+Sistema de Egressos do meu Projeto de TCC para o meu curso de TADS no IFSP
+
+## Banco
+### Como criar o schema e popular o banco para testes:
+- Executar o comando:
+```
+npm run populate
+```
+
+## Backend
+### Como executar
+- Executar o seguinte comando no terminal(na pasta backend):
+```
+npm i
+npm start
+```
---
+++ database/db_structure.sql
@@ -0,0 +1,108 @@
+CREATE TABLE `user` (
+  `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(255) UNIQUE NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `is_admin` TINYINT NOT NULL DEFAULT 0,
+  `created` datetime NOT NULL DEFAULT (CURRENT_TIMESTAMP) COMMENT 'date this record was created',
+  `modified` timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+  `deleted` datetime DEFAULT null COMMENT 'date this record was deleted'
+);
+
+CREATE TABLE `course` (
+  `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `created` datetime NOT NULL DEFAULT (CURRENT_TIMESTAMP) COMMENT 'date this record was created',
+  `modified` timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP)
+);
+
+CREATE TABLE `user_course` (
+  `user_id` INT NOT NULL,
+  `course_id` INT NOT NULL,
+  PRIMARY KEY (`user_id`, `course_id`)
+);
+
+CREATE TABLE `form` (
+  `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
+  `course_id` INT NOT NULL,
+  `title` VARCHAR(255) NOT NULL,
+  `description` TEXT,
+  `created` datetime NOT NULL DEFAULT (CURRENT_TIMESTAMP) COMMENT 'date this record was created',
+  `modified` timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+  `deleted` datetime DEFAULT null COMMENT 'date this record was deleted'
+);
+
+CREATE TABLE `question` (
+  `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `form_id` INT NOT NULL,
+  `text` TEXT NOT NULL,
+  `answer_type` ENUM ('text', 'single_choice', 'multiple_choice', 'date') NOT NULL DEFAULT 'text',
+  `created` datetime NOT NULL DEFAULT (CURRENT_TIMESTAMP) COMMENT 'date this record was created',
+  `modified` timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+  `deleted` datetime DEFAULT null COMMENT 'date this record was deleted'
+);
+
+CREATE TABLE `possible_answer` (
+  `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `question_id` INT NOT NULL,
+  `text` VARCHAR(255) NOT NULL,
+  `created` datetime NOT NULL DEFAULT (CURRENT_TIMESTAMP) COMMENT 'date this record was created',
+  `modified` timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+  `deleted` datetime DEFAULT null COMMENT 'date this record was deleted'
+);
+
+CREATE TABLE `question_dependency` (
+  `question_id` INT NOT NULL,
+  `dependent_question_id` INT NOT NULL,
+  `possible_answer_id` INT NOT NULL,
+  `created` datetime NOT NULL DEFAULT (CURRENT_TIMESTAMP) COMMENT 'date this record was created',
+  `modified` timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+  `deleted` datetime DEFAULT null COMMENT 'date this record was deleted'
+);
+
+CREATE TABLE `form_answer` (
+  `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `form_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
+  `created` datetime NOT NULL DEFAULT (CURRENT_TIMESTAMP) COMMENT 'date this record was created',
+  `modified` timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+  `deleted` datetime DEFAULT null COMMENT 'date this record was deleted'
+);
+
+CREATE TABLE `answer` (
+  `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `question_id` INT NOT NULL,
+  `possible_answer_id` INT,
+  `form_answer_id` INT NOT NULL,
+  `text` TEXT,
+  `created` datetime NOT NULL DEFAULT (CURRENT_TIMESTAMP) COMMENT 'date this record was created',
+  `modified` timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+  `deleted` datetime DEFAULT null COMMENT 'date this record was deleted'
+);
+
+ALTER TABLE `user_course` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
+
+ALTER TABLE `user_course` ADD FOREIGN KEY (`course_id`) REFERENCES `course` (`id`) ON DELETE CASCADE;
+
+ALTER TABLE `form` ADD CONSTRAINT `form_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
+
+ALTER TABLE `question` ADD CONSTRAINT `question_ibfk_1` FOREIGN KEY (`form_id`) REFERENCES `form` (`id`) ON DELETE CASCADE;
+
+ALTER TABLE `possible_answer` ADD CONSTRAINT `possible_answer_ibfk_1` FOREIGN KEY (`question_id`) REFERENCES `question` (`id`) ON DELETE CASCADE;
+
+ALTER TABLE `question_dependency` ADD CONSTRAINT `question_dependency_ibfk_1` FOREIGN KEY (`question_id`) REFERENCES `question` (`id`) ON DELETE CASCADE;
+
+ALTER TABLE `question_dependency` ADD CONSTRAINT `question_dependency_ibfk_2` FOREIGN KEY (`dependent_question_id`) REFERENCES `question` (`id`) ON DELETE CASCADE;
+
+ALTER TABLE `question_dependency` ADD CONSTRAINT `question_dependency_ibfk_3` FOREIGN KEY (`possible_answer_id`) REFERENCES `possible_answer` (`id`) ON DELETE CASCADE;
+
+ALTER TABLE `form_answer` ADD CONSTRAINT `form_answer_ibfk_1` FOREIGN KEY (`form_id`) REFERENCES `form` (`id`) ON DELETE CASCADE;
+
+ALTER TABLE `form_answer` ADD CONSTRAINT `form_answer_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
+
+ALTER TABLE `answer` ADD CONSTRAINT `answer_ibfk_1` FOREIGN KEY (`question_id`) REFERENCES `question` (`id`) ON DELETE CASCADE;
+
+ALTER TABLE `answer` ADD CONSTRAINT `answer_ibfk_2` FOREIGN KEY (`possible_answer_id`) REFERENCES `possible_answer` (`id`) ON DELETE CASCADE;
+
+ALTER TABLE `answer` ADD CONSTRAINT `answer_ibfk_3` FOREIGN KEY (`form_answer_id`) REFERENCES `form_answer` (`id`) ON DELETE CASCADE;
